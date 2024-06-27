8e65b96961a270a2e627ce7f0bc867803d14a791
sukaina55 <talibisoukaina508@gmail.com>
2024-06-27 14:31:38 +0100

    first commit

---
+++ src/main/java/ma/mak/tryyyy/TryyyyApplication.java
@@ -0,0 +1,13 @@
+package ma.mak.tryyyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryyyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TryyyyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/mak/tryyyy/TryyyyApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.mak.tryyyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryyyyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
